80c6ec0749b8670edaffa30930fcccc33dfa7fbb
CLEITON MOURA LOURA <cleitonmloura@gmail.com>
2018-11-15 16:56:09 -0200

    Control Flow Finally

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Speech.Synthesis;
+using System.IO;
 
 namespace Grades
 {
@@ -14,6 +15,39 @@ namespace Grades
 
             GradeBook book = new GradeBook();
 
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+
+        }
+
+        private static void WriteResults(GradeBook book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest Grade", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        private static void SaveGrades(GradeBook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(GradeBook book)
+        {
             try
             {
                 Console.WriteLine("Enter a name");
@@ -23,28 +57,7 @@ namespace Grades
             {
                 Console.WriteLine(ex.Message);
             }
-            catch(NullReferenceException ex)
-            {
-                Console.WriteLine("Something went wrong!");
-            }
-            catch(Exception)
-            {
-                Console.WriteLine("Something went wrong!");
-            }
-
-            book.AddGrade(91);
-            book.AddGrade(89.5f);
-            book.AddGrade(75);
-            book.WriteGrades(Console.Out);
-
-            GradeStatistics stats = book.ComputeStatistics();
-            WriteResult("Average", stats.AverageGrade);
-            WriteResult("Highest", (int)stats.HighestGrade);
-            WriteResult("Lowest Grade", stats.LowestGrade);
-            WriteResult(stats.Description, stats.LetterGrade);
-
         }
-
 
         static void WriteResult(string description, string result)
         {
